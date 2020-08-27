e0c106648f1aaf1beb6cbf395e34a80e7ea65af3
med-tarek-cs <med.tarek.cs@gmail.com>
2020-08-28 00:52:49 +0100

    create some components (form + table + layout)

--- src/router/index.js
+++ src/router/index.js
@@ -10,15 +10,25 @@ Vue.use(VueRouter)
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    {
+      path: '/about',
+      name: 'About',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+      path: '/bookForm',
+      name: 'BookForm',
+      component: () => import('@/components/BookForm')
+    },
+    {
+      path: '/books',
+      name: 'BookList',
+      component: () => import('@/components/BookList')
+    }
+  ]
 
 const router = new VueRouter({
   mode: 'history',
