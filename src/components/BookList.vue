6dbd91eb9e0c6e3fc86d8b31bb6d9a0b78cbb552
med-tarek-cs <med.tarek.cs@gmail.com>
2020-08-28 18:54:24 +0100

    adapting BookList to be able to consume its items from data.json

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -5,49 +5,63 @@
             :items="items"
             :items-per-page="5"
             class="elevation-1"
-
     >
       <template #item.image="{ item }">
-        <v-img
-                :src="item.image"
+        <v-img v-if="item.image"
+                :src="require(`@/assets/${item.image}`)"
                 class="my-3"
                 contain
                 height="60"
                 width="60"
         />
+        <v-img v-else
+               src="https://loremflickr.com/40/60"
+               class="my-3"
+               contain
+               height="60"
+               width="60"
+        />
 
       </template>
 
-      <template v-slot:item.actions="{ item }">
-        <v-icon
+      <template #item.lang="{ item }" >
 
-                class="mr-2"
-                @click="editItem(item)"
-                color="primary"
-        >
-          mdi-pencil
-        </v-icon>
-        |
-        <v-icon
+          <span style="padding: 5px 15px;" > {{ item.lang }} </span>
 
-                @click="deleteItem(item)"
-                color="warning"
-        >
-          mdi-delete
-        </v-icon>
+      </template>
+      <template v-slot:item.actions="{ item }" >
+       <div style="width: 80px">
+         <v-icon
+
+                 class="mr-2"
+                 @click="editItem(item)"
+                 color="primary"
+         >
+           mdi-pencil
+         </v-icon>
+         |
+         <v-icon
+
+                 @click="deleteItem(item)"
+                 color="warning"
+         >
+           mdi-delete
+         </v-icon>
+       </div>
       </template>
     </v-data-table>
   </v-card>
 </template>
 
 <script>
+  import Items from '../../data.json'
   export default {
     name: "BookList",
     data() {
       return {
         headers: [
           {
-            text: 'image',
+            text: 'Image',
             align: 'start',
             sortable: false,
             value: 'image',
@@ -59,16 +73,7 @@
           {text: 'Category', value: 'category'},
           {text: 'Actions', value: 'actions'},
         ],
-        items: [
-          {
-            image: require('@/assets/Practical-Test-Automation.jpg'),
-            title: 'Practical Test Automation',
-            author: 'Panos Matsinopoulos',
-            isbn: '1484261402',
-            lang: 'English',
-            category: 'javascript'
-          }
-        ],
+        items: Items.books
       }
     },
     methods: {
