c37c51df823e6e5b7886e299c1715df880cc8e41
Ilyas Bendaas <ilyesben93@live.fr>
2020-09-01 02:04:21 +0100

    do some changes on navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,66 +1,64 @@
 <template>
   <v-app-bar
-          :clipped-left="$vuetify.breakpoint.lgAndUp"
-          app
-          color="blue darken-3"
-          dark
+    :clipped-left="$vuetify.breakpoint.lgAndUp"
+    app
+    color="blue darken-3"
+    dark
+    src="@/assets/books.jpg"
   >
+
+    <!-- gradient of the navbar background -->
+    <template v-slot:img="{ props }">
+      <v-img
+        v-bind="props"
+        gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+      ></v-img>
+    </template>
+
     <v-app-bar-nav-icon @click.stop="toggleDrawer"></v-app-bar-nav-icon>
-    <v-toolbar-title
-            style="width: 300px"
-            class="ml-0 pl-4"
-    >
-      <span class="hidden-sm-and-down">{{ title}}</span>
+    <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+      <span class="hidden-sm-and-down">{{ title }}</span>
     </v-toolbar-title>
     <v-text-field
-            flat
-            solo-inverted
-            hide-details
-            prepend-inner-icon="mdi-magnify"
-            label="Search"
-            class="hidden-sm-and-down"
+      flat
+      solo-inverted
+      hide-details
+      prepend-inner-icon="mdi-magnify"
+      label="Search"
+      class="hidden-sm-and-down"
     ></v-text-field>
     <v-spacer></v-spacer>
-    <v-btn icon>
-      <v-icon>mdi-apps</v-icon>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>mdi-bell</v-icon>
-    </v-btn>
-    <v-btn
-            icon
-            large
-    >
-      <v-avatar
-              size="32px"
-              item
-      >
-        <v-img
-                src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
-                alt="Vuetify"
-        ></v-img>
-      </v-avatar>
-    </v-btn>
+
+    <!-- looping the icons in the left of navbar -->
+    <div v-for="link in links" :key="link.text" class="d-none d-md-flex">
+      <v-btn color="transparent" dark router :to="link.route">
+        {{ link.text }}
+      </v-btn>
+    </div>
+
   </v-app-bar>
 </template>
 
 <script>
-  export default {
-    name: "Navbar",
-    props: ['title'],
-    data: () => ({
-      drawer: null
-    }),
-    methods: {
-      toggleDrawer() {
-        this.drawer = !this.drawer;
-        console.log(this.drawer)
-        this.$emit('toggleDrawer', this.drawer)
-      }
-    }
-  }
+export default {
+  name: "Navbar",
+  props: ["title"],
+  data: () => ({
+    drawer: null,
+    links: [
+      { text: "Home", route: "/" },
+      { text: "Book List", route: "/books" },
+      { text: "About Us", route: "/about" },
+    ],
+  }),
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer;
+      console.log(this.drawer);
+      this.$emit("toggleDrawer", this.drawer);
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
